f0d3cc6d5e677ad90dbfedd987f553fd90a5c38f
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2025-05-30 09:47:22 +0530

    Create enum.py

---
+++ YukkiMusic/core/enum.py
@@ -0,0 +1,25 @@
+#
+# Copyright (C) 2024-2025 by TheTeamVivek@Github, < https://github.com/TheTeamVivek >.
+#
+# This file is part of < https://github.com/TheTeamVivek/YukkiMusic > project,
+# and is released under the MIT License.
+# Please see < https://github.com/TheTeamVivek/YukkiMusic/blob/master/LICENSE >
+#
+# All rights reserved.
+#
+
+from enum import Enum, auto
+
+__all__ = [
+    "SourceType",
+]
+
+class SourceType(Enum):
+    APPLE = "Apple"
+    RESSO = "Resson"
+    SAAVN = "JioSaavn"
+    SOUNDCLOUD = "Soundcloud"
+    SPOTIFY = "Spotify"
+    TELEGRAM = "Telegram"
+    YOUTUBE = "YouTube"
+    M3U8 = "M3U8 Urls"
