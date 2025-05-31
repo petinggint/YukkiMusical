19f7e96bec51348bda5cbffb6dc3277934aa82b5
Vivek Kumar <167422483+Vivekkumar-IN@users.noreply.github.com>
2025-05-31 05:44:22 +0530

    Update request.py

--- YukkiMusic/core/request.py
+++ YukkiMusic/core/request.py
@@ -2,27 +2,25 @@ from typing import Any
 
 import aiohttp
 
-__all__ = ["Request"]
+__all__ = ["get_json"]
 
 
-class Request:
-    @staticmethod
-    async def get_json(
-        url: str,
-        params: dict[str, Any] | None = None,
-        headers: dict[str, str] | None = None,
+async def get_json(
+    url: str,
+    params: dict[str, Any] | None = None,
+    headers: dict[str, str] | None = None,
     ) -> dict[str, Any]:
-        """
-        Sends a GET request and returns the response as JSON.
+    """
+    Sends a GET request and returns the response as JSON.
 
-        Args:
-            url (str): The URL to send the GET request to.
-            params (Optional[Dict[str, Any]]): Query parameters to include in the request. Defaults to None.
-            headers (Optional[Dict[str, str]]): Headers to include in the request. Defaults to None.
+    Args:
+        url (str): The URL to send the GET request to.
+        params (Optional[Dict[str, Any]]): Query parameters to include in the request. Defaults to None.
+        headers (Optional[Dict[str, str]]): Headers to include in the request. Defaults to None.
 
-        Returns:
-            Dict[str, Any]: The JSON response from the request.
-        """
-        async with aiohttp.ClientSession() as session:
-            async with session.get(url, params=params, headers=headers) as resp:
-                return await resp.json()
+    Returns:
+        Dict[str, Any]: The JSON response from the request.
+    """
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url, params=params, headers=headers) as resp:
+            return await resp.json()
